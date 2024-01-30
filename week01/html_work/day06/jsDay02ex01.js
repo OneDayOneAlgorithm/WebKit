5bda378b149546b5a8b60f5ecd84ff87094a4414
KimHyoengJin <gudwls9966@naver.com>
2024-01-30 16:49:52 +0900

    1

---
+++ week01/html_work/day06/jsDay02ex01.js
@@ -0,0 +1,20 @@
+// External(외부) 방식의 자바스크립트 사용
+console.log("jsday02ex01.js에 작성된 스크립트")
+
+const numbers = [1,2,3,4,5];
+console.log(numbers);
+
+numbers.push(6);
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+
+numbers.shift();
+console.log(numbers);
+
+numbers.unshift(0);
+console.log(numbers);
+
+numbers.unshift(100,200);
+console.log(numbers);
