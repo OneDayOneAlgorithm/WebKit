60875b0778ceee6855948c3414201c2188d9296f
Kim hyeongjin <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2024-02-09 13:49:28 +0900

    1

--- week03/day12/reactfolder/src/App.js
+++ week03/day12/reactfolder/src/App.js
@@ -1,31 +1,42 @@
 import { useState } from 'react';
 import './App.css';
+import ListBody from './ListBody';
 
 // App에서 사용 할 새 컴포넌트 선언
 // input에 내용을 입력하고 버튼을 클릭하면
 // 입력 된 내용으로 appTitle이 변경 되도록 하라.
 function Header(props) {
-  const [title, setTitle] = useState("");
+  const [newTodo, setNewTodo] = useState("");
   return (<header className="App-header">
-    <h1>{props.appTitle}</h1>
-    <input onChange={(e)=>{setTitle(e.target.value)}} type="text" value={title}></input>
-    <button onClick={props.btnHandler}>Click me!</button>
+    <h1>Todo List</h1>
+    <input onChange={(e)=>{setNewTodo(e.target.value)}} type="text" value={newTodo}></input>
+    <button onClick={()=>{
+      props.btnHandler(newTodo);
+      setNewTodo('');
+    }}>Click me!</button>
   </header>);
 }
 
 // Header 컴포넌트를 사용하는 컴포넌트
 function App() {
-  const [appTitle, setAppTitle] = useState("Hello world");
-  const [cnt, setCnt] = useState(0);
-  const btnHandler = function(e) {
-    // 버튼이 클릭 되면 cnt가 하나씩 증가
-    var newCnt = cnt+1;
-    console.log("click:", newCnt);
-    setCnt(newCnt);
+  const [todoList,setTodoList] = useState([]);
+
+  const btnHandler = function(newTodo,index) {
+
+    setTodoList(prevTodoList => [...prevTodoList,newTodo]);
+
+    console.log('하이')
+  }
+  const todoDelete = function(todo,key){
+    // setTodoList(prevTodoList => prevTodoList.filter((todo,key)=> ))
   }
   return (
     <div className="App">
-      <Header appTitle={appTitle} btnHandler={btnHandler}></Header>
+      <Header btnHandler={btnHandler}></Header>
+      {todoList.map((todo,index)=>{
+        return(<ListBody todo={todo} key={index} todoDelete={todoDelete}></ListBody>)
+      })}
+      
     </div>
   );
 }
--- week03/day12/reactfolder/src/ListBody.js
+++ week03/day12/reactfolder/src/ListBody.js
@@ -1,14 +1,24 @@
-function ListBody(){
+function ListBody(props){
+    const todoDelete = {
+
+    }
+    console.dir(props)
     return (<div>
       <div>
         <input type='checkbox'/>
-        <span>해야 할 일</span>
+        <span>{props.todo}</span>
         <button>Edit</button>
-        <button>Delete</button>
+        <button onClick = {()=>{
+          props.todoDelete(props.todo,props.key)
+        }}>Delete</button>
       </div>
       <hr/>
     </div>);
   }
+
+function todoDelete(props){
+  return;
+}
 
 // export로 등록한 컴포넌트를 사용 할 때는 import 해준다.
 export default ListBody;
